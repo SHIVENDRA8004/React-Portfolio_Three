09a7be9eb67de60e48da8143eb559093613e3caa
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-11 01:13:29 +0530

    Error occuring in file types

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../style";
-import { navLinks } from "../constants";
-import { logo, menu, close } from "../assets";
+import { navLinks } from "../constants/constants";
+import { logo, menu, close } from "../assets/";
 
 const Navbar = () => {
   return <div>Navbar</div>;
