b467ebd8d355a1ef1705d24c7b6f8b33713e079b
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-10 23:53:12 +0530

    Added basic code in app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 const App = () => {
   return (
-    <div className="App">
-      <h2>3D developer Portfolio</h2>
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">Hello</div>
+    </BrowserRouter>
   );
 };
 
