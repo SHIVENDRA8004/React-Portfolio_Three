3af417659262ef9a615eeae3b5c6f4d8448041e9
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-11 01:01:31 +0530

    Added all components list wise in app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
     </BrowserRouter>
   );
