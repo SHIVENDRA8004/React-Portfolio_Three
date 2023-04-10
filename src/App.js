cff1112a8f5ed6c0deb60ae99791197d715deb67
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-11 00:59:04 +0530

    Added navbar and hero section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">Hello</div>
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
       </div>
     </BrowserRouter>
   );
